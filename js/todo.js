ec1de3ffc4e2315d55df1eb8d5007171b86d07ee
Bogdan <gifutolord@gmail.com>
2022-01-22 04:42:36 +0200

    add checkbox; add style(2.0);

--- js/todo.js
+++ js/todo.js
@@ -1,6 +1,7 @@
 const field = document.querySelector('.field');
 const list = document.querySelector('.list');
 const btn = document.querySelector('.add');
+const check = document.querySelector('.task')
 
 function createTask(value) {
     const task = document.createElement('div');
@@ -12,9 +13,18 @@ function createTask(value) {
 function addTask() {
     if (field.value != null) {
         const newTask = createTask(field.value);
+        const newCheckBox = createChecBox();
+        list.appendChild(newCheckBox)
         list.appendChild(newTask);
         field.value = '';
     }     
+}
+
+function createChecBox(value) {
+    const checkBox = document.createElement("INPUT");
+    checkBox.setAttribute("type", "checkbox");
+    checkBox.classList.add('checkbox');
+    return checkBox;
 }
 
 btn.addEventListener('click', addTask);
